d9f49546f00d1afe112cbd578093e47e067225b9
Gulbeyaz <glbzbayram@gmail.com>
2023-10-08 15:17:48 +0300

    Main.js was created

---
+++ e-commerce/src/layouts/Main.js
@@ -0,0 +1,15 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import NavBar from "./Navbar";
+
+const Main = ({}) => {
+  return (
+    <div>
+      <NavBar />
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
