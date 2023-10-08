f4a5a7758379d37ccd6b46f3c5886196104e39bb
Gulbeyaz <glbzbayram@gmail.com>
2023-10-08 17:36:39 +0300

    shopCards component was created

---
+++ e-commerce/src/components/ShopCards.js
@@ -0,0 +1,50 @@
+import banner1 from "../Assets/h5-baner-3 1baner-3.png";
+import banner2 from "../Assets/h5-baner-1 1.png";
+import banner3 from "../Assets/h5-baner-1 1 (1).png";
+
+const ShopCards = () => {
+  return (
+    <div className="container m-auto">
+      <div className="row flex">
+        <div className="col-md-4 w-[332px] h-[232px]">
+          <div className="relative">
+            <img className="relative z-0" src={banner1}></img>
+            <div className=" absolute z-50  top-20 left-10 ">
+              <h6 className="text-xs font-normal">Your Space</h6>
+              <h2 className="text-2xl font-bold w-[94px] h-[64px]">
+                Unique Life
+              </h2>
+              <h6 className="text-xs font-normal">Explore Items</h6>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4 w-[332px] h-[232px]">
+          <div className="relative">
+            <img className="relative z-0" src={banner2}></img>
+            <div className=" absolute z-50  top-20 left-10 ">
+              <h6 className="text-xs font-normal">Ends Today</h6>
+              <h2 className="text-2xl font-bold w-[94px] h-[64px]">
+                Elements Style
+              </h2>
+              <h6 className="text-xs font-normal">Explore Items</h6>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4 w-[332px] h-[232px]">
+          <div className="relative">
+            <img className="relative z-0" src={banner3}></img>
+            <div className=" absolute z-50 top-20 left-10 ">
+              <h6 className="text-xs font-normal">Ends Today</h6>
+              <h2 className="text-2xl font-bold w-[94px] h-[64px]">
+                Elements Style
+              </h2>
+              <h6 className="text-xs font-normal">Explore Items</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShopCards;
