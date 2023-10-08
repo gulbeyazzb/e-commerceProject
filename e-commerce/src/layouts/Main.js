988b2cbd067076b493fd5786c4f5322b2aa3a454
Gulbeyaz <glbzbayram@gmail.com>
2023-10-09 02:06:41 +0300

    Components were connected to Main Page

--- e-commerce/src/layouts/Main.js
+++ e-commerce/src/layouts/Main.js
@@ -1,3 +1,7 @@
+import BestSeller from "../components/BestSeller";
+import Clients from "../components/Clients";
+import ProductCards from "../components/ProductCards";
+import ProductCards2 from "../components/ProductCards2";
 import ShopCards from "../components/ShopCards";
 import Footer from "./Footer";
 import Header from "./Header";
@@ -9,6 +13,10 @@ const Main = ({}) => {
       <NavBar />
       <Header />
       <ShopCards />
+      <ProductCards />
+      <ProductCards2 />
+      <BestSeller />
+      <Clients />
       <Footer />
     </div>
   );
