4ef8f89b799c537dd7239e559ca5f7c737e20b6f
Gulbeyaz <glbzbayram@gmail.com>
2023-10-11 23:44:16 +0300

    fake data was created to pull of products

---
+++ e-commerce/src/mocks/data.js
@@ -0,0 +1,75 @@
+import Pcard1 from "../Assets/clothes/product-cover-5 (7).png";
+import Pcard2 from "../Assets/clothes/product-cover-5 (8).png";
+import Pcard3 from "../Assets/clothes/product-cover-5 (9).png";
+import Pcard4 from "../Assets/clothes/product-cover-5 (10).png";
+import Pcard5 from "../Assets/clothes/product-cover-5 (11).png";
+import Pcard6 from "../Assets/clothes/product-cover-5 (12).png";
+import Pcard7 from "../Assets/clothes/product-cover-5 (13).png";
+import Pcard8 from "../Assets/clothes/product-cover-5 (14).png";
+import Pcard9 from "../Assets/clothes/product-cover-5 (15).png";
+import Pcard10 from "../Assets/clothes/product-cover-5 (16).png";
+import Pcard11 from "../Assets/clothes/product-cover-5 (17).png";
+import Pcard12 from "../Assets/clothes/product-cover-5 (7).png";
+
+export let data = [
+  {
+    id: 0,
+    src: Pcard1,
+    heading: " Graphic Design",
+  },
+  {
+    id: 1,
+    src: Pcard2,
+    heading: " Graphic Design",
+  },
+  {
+    id: 2,
+    src: Pcard3,
+    heading: " Graphic Design",
+  },
+  {
+    id: 3,
+    src: Pcard4,
+    heading: " Graphic Design",
+  },
+  {
+    id: 4,
+    src: Pcard5,
+    heading: " Graphic Design",
+  },
+  {
+    id: 5,
+    src: Pcard6,
+    heading: " Graphic Design",
+  },
+  {
+    id: 6,
+    src: Pcard7,
+    heading: " Graphic Design",
+  },
+  {
+    id: 7,
+    src: Pcard8,
+    heading: " Graphic Design",
+  },
+  {
+    id: 8,
+    src: Pcard9,
+    heading: " Graphic Design",
+  },
+  {
+    id: 9,
+    src: Pcard10,
+    heading: " Graphic Design",
+  },
+  {
+    id: 10,
+    src: Pcard11,
+    heading: " Graphic Design",
+  },
+  {
+    id: 11,
+    src: Pcard12,
+    heading: " Graphic Design",
+  },
+];
