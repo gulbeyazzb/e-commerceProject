cafe432fb840cbc4f45dfadb0f1bbbd4e03e57ca
Gulbeyaz <glbzbayram@gmail.com>
2023-10-11 23:45:12 +0300

    Main.js content was changed.

--- e-commerce/src/layouts/Main.js
+++ e-commerce/src/layouts/Main.js
@@ -1,28 +1,12 @@
-import BestSeller from "../components/BestSeller";
-import Clients from "../components/Clients";
-import FeaturedPosts from "../components/FeaturedPosts";
-import MostPopularProducts from "../components/MostPopularProducts";
-import MostPopularProducts2 from "../components/MostPopularProducts2";
-import ProductCards from "../components/ProductCards";
-import ProductCards2 from "../components/ProductCards2";
-import ShopCards from "../components/ShopCards";
-import Footer from "./Footer";
-import Header from "./Header";
 import NavBar from "./Navbar";
+import { PageContent } from "./PageContent";
+import Footer from "../layouts/Footer";
 
 const Main = ({}) => {
   return (
     <div className="font-[Montserrat]">
       <NavBar />
-      <Header />
-      <ShopCards />
-      <ProductCards />
-      <MostPopularProducts />
-      <ProductCards2 />
-      <MostPopularProducts2 />
-      <BestSeller />
-      <Clients />
-      <FeaturedPosts />
+      <PageContent />
       <Footer />
     </div>
   );
