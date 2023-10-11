22c4e5d855f8e4e88357877c7e8e0084656719c8
Gulbeyaz <glbzbayram@gmail.com>
2023-10-11 23:45:38 +0300

    ProductHeader was created

---
+++ e-commerce/src/layouts/ProductHeader.js
@@ -0,0 +1,27 @@
+import { NavLink } from "react-router-dom";
+
+const ProductHeader = () => {
+  return (
+    <div className="bg-[#FAFAFA]">
+      <div className="flex justify-between container mx-auto py-6  px-[7.5rem]  h-14">
+        <div>
+          <NavLink to="/product-list" exact className="font-bold text-2xl ">
+            Shop
+          </NavLink>
+        </div>
+
+        <div className="flex text-[#737373] font-bold text-sm items-center">
+          <NavLink to="/" className=" text-[#252B42]" exact>
+            Home
+          </NavLink>
+          <i class="bx bxs-chevron-right text-[#BDBDBD] text-2xl"></i>
+          <NavLink to="/product-list" className="mr-1">
+            Shop
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductHeader;
