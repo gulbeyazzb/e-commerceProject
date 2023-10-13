14108bf0e189815129ef411a2afa74da062d54dc
Gulbeyaz <glbzbayram@gmail.com>
2023-10-13 13:17:22 +0300

    editted

--- e-commerce/src/layouts/Header.js
+++ e-commerce/src/layouts/Header.js
@@ -1,11 +1,9 @@
 import { Carousel } from "@material-tailwind/react";
-import { useState } from "react";
-import data from "../mocks/data";
 
 const Header = () => {
   return (
     <div className="relative">
-      <div className="relative ">
+      <div className=" relative ">
         <Carousel
           navigation={({ setActiveIndex, activeIndex, length }) => (
             <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
@@ -24,12 +22,12 @@ const Header = () => {
           <img
             src="https://s3-alpha-sig.figma.com/img/58e7/8626/57520fbcc340e0540da6a9164dc64c56?Expires=1697414400&Signature=mkKtiwzRXMp7~Pm7BkCBhvayaVdGLoGpRmGjb~AqV6anUNESA4YGZDhbtx9Vum3mrsXDnhrq3rPgbU9nra7WG9WAyoMnHMpM09tB-sVWktvlGYGJo7osX32yX1LlfWywcDlwTDFlt1dwBHdLzu02duJqNDhn8Ap3FyyGxGRzk~Z~f0GQO61uCPgN1C-yrnsJjb~-wPvEaQCWNnguDqSp6MmBXnSBI9mak5iO2SujD5XIQsxUO~rc39kPcOh9tYYVPLsTRk2HD~eEmnFFzjqqNTgOifg11qaqeo428ZbAF2SaOQrvKCsgOxTLTh-JhQ0PO0NFApYP8XlJW7UgmRXbsw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
             alt="image 1"
-            className="h-96 w-full object-cover "
+            className="h-96 w-full object-cover brightness-50 "
           />
           <img
             src="https://s3-alpha-sig.figma.com/img/58e7/8626/57520fbcc340e0540da6a9164dc64c56?Expires=1697414400&Signature=mkKtiwzRXMp7~Pm7BkCBhvayaVdGLoGpRmGjb~AqV6anUNESA4YGZDhbtx9Vum3mrsXDnhrq3rPgbU9nra7WG9WAyoMnHMpM09tB-sVWktvlGYGJo7osX32yX1LlfWywcDlwTDFlt1dwBHdLzu02duJqNDhn8Ap3FyyGxGRzk~Z~f0GQO61uCPgN1C-yrnsJjb~-wPvEaQCWNnguDqSp6MmBXnSBI9mak5iO2SujD5XIQsxUO~rc39kPcOh9tYYVPLsTRk2HD~eEmnFFzjqqNTgOifg11qaqeo428ZbAF2SaOQrvKCsgOxTLTh-JhQ0PO0NFApYP8XlJW7UgmRXbsw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
             alt="image 2"
-            className="h-96 w-full object-cover"
+            className="h-96 w-full object-cover brightness-50"
           />
         </Carousel>
       </div>
