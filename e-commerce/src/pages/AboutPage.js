3a712d673800d1744263eb427d9b976e14e65486
Gulbeyaz <glbzbayram@gmail.com>
2023-10-18 13:41:15 +0300

    AboutPage and ContactPage were created

--- e-commerce/src/pages/AboutPage.js
+++ e-commerce/src/pages/AboutPage.js
@@ -1,18 +1,24 @@
 import AboutContent from "../components/About/AboutContent";
 import AboutHeader from "../components/About/AboutHeader";
 import AboutStats from "../components/About/AboutStats";
+import { AboutTeam } from "../components/About/AboutTeam";
 import { AboutVideo } from "../components/About/AboutVideo";
+import AboutClients from "../components/About/AboutClients";
+import AboutTestimonials from "../components/About/AboutTestimonials";
 
 const AboutPage = () => {
   return (
-    <div className="py-36">
-      <div className=" container m-auto">
+    <>
+      <div className=" w-[1000px] m-auto">
         <AboutHeader />
         <AboutContent />
         <AboutStats />
         <AboutVideo />
+        <AboutTeam />
       </div>
-    </div>
+      <AboutClients />
+      <AboutTestimonials />
+    </>
   );
 };
 export default AboutPage;
---
+++ e-commerce/src/pages/ContactPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ContactHeader from "../components/Contact/ContactHeader";
+import Contact from "../components/Contact/Contact";
+const ContactPage = () => {
+  return (
+    <div className=" w-[1000px] m-auto">
+      <ContactHeader />
+      <Contact />
+    </div>
+  );
+};
+export default ContactPage;
