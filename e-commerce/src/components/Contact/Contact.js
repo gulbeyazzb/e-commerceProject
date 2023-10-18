d7ec3edb4f761e9b944a3604bacfe3f9a00bf70a
Gulbeyaz <glbzbayram@gmail.com>
2023-10-18 13:40:33 +0300

    moved to client

---
+++ e-commerce/src/components/Contact/Contact.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <div>
+      <div className="flex flex-col gap-20 py-28 text-center items-center">
+        <div>
+          <div className="flex flex-col gap-[10px] font-bold">
+            <h6 className="text-sm">VISIT OUR OFFICE</h6>
+            <h2 className="w-[500px] text-4xl">
+              We help small businesses with big ideas
+            </h2>
+          </div>
+        </div>
+        <div>
+          <div className="h-[343px]"></div>
+          <div className="h-[403px]"></div>
+          <div className="h-[343px]"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
