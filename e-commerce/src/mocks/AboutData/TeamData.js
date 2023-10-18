219619c0c9dcd2424b7ca22a370d0d3be57c4de4
Gulbeyaz <glbzbayram@gmail.com>
2023-10-18 13:42:00 +0300

    fake data was created

---
+++ e-commerce/src/mocks/AboutData/TeamData.js
@@ -0,0 +1,30 @@
+import teamMember1 from "../../Assets/about/team/team-1-user-1.jpg";
+import teamMember2 from "../../Assets/about/team/team-1-user-2.jpg";
+import teamMember3 from "../../Assets/about/team/team-1-user-3.jpg";
+
+export const TeamData = [
+  {
+    img: teamMember1,
+    header: "username",
+    role: "professsion",
+    facebook: "",
+    instagram: "",
+    twitter: "",
+  },
+  {
+    img: teamMember2,
+    header: "username",
+    role: "professsion",
+    facebook: "",
+    instagram: "",
+    twitter: "",
+  },
+  {
+    img: teamMember3,
+    header: "username",
+    role: "professsion",
+    facebook: "",
+    instagram: "",
+    twitter: "",
+  },
+];
