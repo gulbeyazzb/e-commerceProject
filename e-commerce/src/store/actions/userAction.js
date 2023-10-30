52982eab5f57110950b88145ab6ae8ae2e0e1b25
Gulbeyaz <glbzbayram@gmail.com>
2023-10-30 04:30:28 +0300

    fetchstate was dispatched

--- e-commerce/src/store/actions/userAction.js
+++ e-commerce/src/store/actions/userAction.js
@@ -1,6 +1,32 @@
-export const SET_USER = "SET_USER";
+import { toast } from "react-toastify";
+import { axiosWithAuth } from "../../utilities/axiosWithAuth";
+import { FETCH_STATES } from "../reducers/productReducer";
 
-export const setUserCustomerAction = (user) => ({
-  type: SET_USER,
-  payload: user,
-});
+export const SET_USER = "SET_USER";
+export const SET_USER_FETCH_STATE = "SET_USER_FETCH_STATE";
+
+export const fetchUserActionCreator =
+  (userFormData) => (dispatch, getState) => {
+    if (getState().user.fetchState === FETCH_STATES.NotFetched) {
+      dispatch({ type: SET_USER_FETCH_STATE, payload: FETCH_STATES.Fetching });
+      axiosWithAuth()
+        .post("login", userFormData)
+        .then((res) => {
+          dispatch({ type: SET_USER, payload: res.data });
+          dispatch({
+            type: SET_USER_FETCH_STATE,
+            payload: FETCH_STATES.Fetched,
+          });
+          console.log("action:", res);
+          toast.success(res.data.message);
+        })
+        .catch((err) => {
+          dispatch({
+            type: SET_USER_FETCH_STATE,
+            payload: FETCH_STATES.FetchFailed,
+          });
+          console.error(err);
+          toast.warning(err.response.data.message);
+        });
+    }
+  };
