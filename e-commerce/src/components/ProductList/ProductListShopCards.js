c0937d3a2b1b2b564ffc81f1213a2374d8f4d6d9
Gulbeyaz <glbzbayram@gmail.com>
2023-10-31 19:40:37 +0300

    categories was fetched

--- e-commerce/src/components/ProductList/ProductListShopCards.js
+++ e-commerce/src/components/ProductList/ProductListShopCards.js
@@ -1,20 +1,26 @@
-import { shopCards } from "../../mocks/productList/shopCards";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { fetchCategoryActionCreator } from "../../store/actions/globalAction";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
-const ProductListShopCards = () => {
+const ProductListShopCards = ({ categories }) => {
+  console.log("categories:", categories);
   return (
     <div className="bg-[#FAFAFA]">
       <div className=" flex justify-center">
-        <div className="mobile-col-flex sm:gap-[10px] gap-[18px] sm:pb-12 items-center text-white text-center ">
-          {shopCards.map((card) => (
-            <div>
-              <div className="relative">
-                <img className="relative z-0 " src={card.src}></img>
+        <div className="mobile-col-flex flex-wrap justify-center sm:gap-[10px] gap-[18px] sm:pb-12 items-center text-white text-center ">
+          {categories.slice(0, 5).map((category) => (
+            <NavLink to={`/shopping/${category.code}`}>
+              <div className="relative hover:brightness-50">
+                <img
+                  className="relative z-0 h-[15rem]"
+                  src={category.img}
+                ></img>
                 <div className=" absolute z-50  top-[40%] left-[30%] ">
-                  <h2 className="text-base font-bold">{card.heading}</h2>
-                  <h6 className="text-sm font-normal">{card.subHeading}</h6>
+                  <h2 className="text-base font-bold">{category.title}</h2>
                 </div>
               </div>
-            </div>
+            </NavLink>
           ))}
         </div>
       </div>
