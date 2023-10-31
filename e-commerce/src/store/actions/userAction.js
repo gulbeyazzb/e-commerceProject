44f9be2a2c85af6621da7e53c12b2d84c27a2b8a
Gulbeyaz <glbzbayram@gmail.com>
2023-10-31 19:40:05 +0300

    token was added localstorage

--- e-commerce/src/store/actions/userAction.js
+++ e-commerce/src/store/actions/userAction.js
@@ -12,13 +12,12 @@ export const fetchUserActionCreator =
       API.post("login", userFormData)
         .then((res) => {
           dispatch({ type: SET_USER, payload: res.data });
-          localStorage.setItem("token", res.data.token);
           console.log("thunk res(signup verileri dönüyor):", res);
           dispatch({
             type: SET_USER_FETCH_STATE,
             payload: FETCH_STATES.Fetched,
           });
-          renewAPI();
+          renewAPI(res.data.token);
 
           console.log("action:", res);
           toast.success(res.data.message);
@@ -33,3 +32,15 @@ export const fetchUserActionCreator =
         });
     }
   };
+
+export const getUserVerifyAction = () => (dispatch) => {
+  API.get("verify")
+    .then((res) => {
+      dispatch({ type: SET_USER, payload: res.data });
+      renewAPI(res.data.token);
+    })
+    .catch((err) => {
+      console.error("verifythunaction error:", err);
+      renewAPI();
+    });
+};
