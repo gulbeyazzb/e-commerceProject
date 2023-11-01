2f0bee8105501bdd8221de7e988430974b4ba4f8
Gulbeyaz <glbzbayram@gmail.com>
2023-11-01 16:25:48 +0300

    filter

--- e-commerce/src/components/ProductList/FilterComponent.js
+++ e-commerce/src/components/ProductList/FilterComponent.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Select, Option } from "@material-tailwind/react";
 
 export const FilterComponent = () => {
+  const [filter, setFilter] = useState();
+  const changeOptionHandle = (e) => {
+    const selectedFilter = e.target.value;
+    setFilter(selectedFilter);
+  };
+
   return (
     <div className="py-6 ">
       <div className="mobile-col-flex justify-center items-center gap-6 sm:gap-[12rem] py-6 sm:py-0">
@@ -19,13 +25,39 @@ export const FilterComponent = () => {
         </div>
         <div className="flex items-center gap-10">
           <div>
-            <Select label="Popularity" className="text-[#DDDDDD] bg-[#F9F9F9] ">
-              <Option>Skirt</Option>
-              <Option>Pants</Option>
-              <Option>Jean</Option>
-              <Option>Shoes</Option>
-              <Option>Dress</Option>
-            </Select>
+            <select
+              className="p-4 rounded-md border border-[#DADADA] text-black"
+              onClick={(e) => changeOptionHandle(e)}
+            >
+              <option
+                key="worstRated"
+                value="worstRated"
+                className=" text-lg font-bold"
+              >
+                Worst Rated
+              </option>
+              <option
+                key="bestRated"
+                value="bestRated"
+                className=" text-lg font-bold"
+              >
+                Best Rated
+              </option>
+              <option
+                key="priceAsc"
+                value="priceAsc"
+                className=" text-lg font-bold"
+              >
+                Increasing by Price
+              </option>
+              <option
+                key="priceDesc"
+                value="priceDesc"
+                className=" text-lg font-bold"
+              >
+                decreasing by price
+              </option>
+            </select>
           </div>
           <button className="bg-primary-color py-[10px] px-5 rounded text-[#FFFFFF]">
             Filter
