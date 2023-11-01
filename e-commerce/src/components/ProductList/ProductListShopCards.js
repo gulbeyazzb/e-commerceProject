fea231014fbcde27a4aa7fcb0b493413a67418fd
Gulbeyaz <glbzbayram@gmail.com>
2023-11-01 16:27:27 +0300

    categories dispatched

--- e-commerce/src/components/ProductList/ProductListShopCards.js
+++ e-commerce/src/components/ProductList/ProductListShopCards.js
@@ -1,15 +1,27 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { fetchCategoryActionCreator } from "../../store/actions/globalAction";
 import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
-const ProductListShopCards = ({ categories }) => {
+const ProductListShopCards = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCategoryActionCreator());
+  }, []);
+  const categories = useSelector((store) => store.global.categories);
+
+  const mostRating = categories.sort((a, b) => {
+    return b.rating - a.rating;
+  });
+  console.log("most:", mostRating);
+
   return (
     <div className="bg-[#FAFAFA]">
       <div className=" flex justify-center">
         <div className="mobile-col-flex flex-wrap justify-center sm:gap-[10px] gap-[18px] sm:pb-12 items-center text-white text-center ">
-          {categories.slice(0, 5).map((category) => (
-            <NavLink to={`/shopping/${category.code}`}>
+          {mostRating.slice(0, 6).map((category) => (
+            <NavLink to={`/shopping/${category.id}`}>
               <div className="relative hover:brightness-50">
                 <img
                   className="relative z-0 sm:h-[15rem] w-full sm:w-auto px-2 sm:p-0"
