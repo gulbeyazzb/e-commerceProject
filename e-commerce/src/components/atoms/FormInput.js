a38c76610de2bfb6e4ca0f36b3b2f4bfbbfcc0db
Gulbeyaz <glbzbayram@gmail.com>
2023-11-12 14:54:34 +0300

    deleted

--- e-commerce/src/components/atoms/FormInput.js
+++
@@ -1,9 +0,0 @@
-import { Input } from "reactstrap";
-
-const FormInput = ({ register, name, validations, ...rest }) => {
-  const { ref, ...registerField } = register(name, validations);
-
-  return <Input innerRef={ref} {...registerField} {...rest} />;
-};
-
-export default FormInput;
