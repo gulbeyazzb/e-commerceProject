64722ff39865e5b681d600f01a4e01600fb6a7c4
Gulbeyaz <glbzbayram@gmail.com>
2023-12-01 14:14:13 +0300

    HomePage route has been editted

--- e-commerce/src/layouts/PageContent.js
+++ e-commerce/src/layouts/PageContent.js
@@ -1,6 +1,6 @@
 import { Route, Switch } from "react-router-dom";
 import ProductListPage from "../pages/ProductListPage";
-import HomePage from "../pages/HomePage";
+import HomePage from "../pages//HomePage";
 import AboutPage from "../pages/AboutPage";
 import ContactPage from "../pages/ContactPage";
 import ProductPage from "../pages/ProductPage";
