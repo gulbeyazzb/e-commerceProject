dab8ff39c0c11fddac5b4f85ea84504ab7385e2f
Gulbeyaz <glbzbayram@gmail.com>
2023-12-01 17:30:43 +0300

    mobile products ux design

--- e-commerce/src/components/ProductList/Products.js
+++ e-commerce/src/components/ProductList/Products.js
@@ -37,13 +37,14 @@ const Products = ({
     }
     toast.success("the product has been added to cart");
   };
+
   return (
     <div className=" w-full ">
       <div className="flex flex-col gap-4 w-full">
         <div className="flex justify-center items-center">
           {productFetching && <Spinner />}
         </div>
-
+        {/* //WEB SAYFAM */}
         {productFetched && (
           <div className="hidden sm:flex flex-wrap gap-8 justify-center items-center">
             {products?.map((product) => (
@@ -154,94 +155,70 @@ const Products = ({
             ))}
           </div>
         )}
-
-        <div className="sm:hidden flex flex-col gap-[30px] w-full px-4">
-          {/* {mobileProducts?.map((product) => (
-            <Link
-              to={`/product/${categoryPath}${product.name}${product.id}`}
-              className="flex flex-col text-center gap-[2rem] mb-10 justify-center items-center"
-              onClick=""
+        {/* //MOBILE SAYFAM */}
+        <div className="sm:hidden flex flex-wrap justify-between">
+          {products?.map((product) => (
+            <Card
+              className="flex flex-col w-[186px] items-between mb-3"
               key={product.id}
-              id={product.id}
             >
-              <Card className="flex flex-col w-96 h-[50rem] gap-1 justify-center items-center">
-                <CardHeader className=" justify-center items-center">
+              {" "}
+              <Link
+                to={`/product/${product.id}/${product.name
+                  .toLowerCase()
+                  .replaceAll(" ", "-")} `}
+                key={product.id}
+                id={product.id}
+              >
+                <div className=" hover:brightness-50 rounded-md line-clamp-1">
                   <img
                     src={product.images[0].url}
                     alt="card-image"
-                    className="h-[25rem] w-[full] object-contain items-center"
+                    className="w-full object-center h-60 object-cover items-center line-clamp-1"
                   />
-                </CardHeader>
-                <CardBody>
-                  <Typography
-                    variant="h5"
-                    color="blue-gray"
-                    className="mb-2"
-                    id={product.name}
-                  >
-                    {product.name}
-                  </Typography>
-                  <Typography className="text-start">
-                    {product.description}
-                  </Typography>
-                  <Typography className="flex justify-between mt-10 text-end font-bold text-orange-800">
-                    {" "}
-                    <div>
-                      <button>
-                        <i className="bx bxs-circle text-primary-color"></i>
-                      </button>
-                      <button>
-                        <i className="bx bxs-circle text-[#23856D]"></i>
-                      </button>
-                      <button>
-                        <i className="bx bxs-circle text-[#E77C40]"></i>
-                      </button>
-                      <button>
-                        <i className="bx bxs-circle text-[#252B42]"></i>
-                      </button>
-                    </div>
-                    {product.price}₺
-                  </Typography>
-                </CardBody>
-                <CardFooter className="flex justify-between gap-2">
+                </div>
+              </Link>
+              <CardBody className="flex flex-col line-clamp-2 gap-1">
+                <Typography className="text-start line-clamp-1">
+                  {product.description}
+                </Typography>
+                <Typography className="flex font-bold text-orange-800 line-clamp-1">
+                  {" "}
                   <div>
-                    {" "}
-                    <select className="p-4 w-32 rounded-md border border-[#DADADA] text-black">
-                      <option
-                        key="worstRated"
-                        value="worstRated"
-                        className=" text-lg font-bold"
-                      >
-                        M
-                      </option>
-                      <option
-                        key="bestRated"
-                        value="bestRated"
-                        className=" text-lg font-bold"
-                      >
-                        S
-                      </option>
-                      <option
-                        key="priceAsc"
-                        value="priceAsc"
-                        className=" text-lg font-bold"
-                      >
-                        L
-                      </option>
-                      <option
-                        key="priceDesc"
-                        value="priceDesc"
-                        className=" text-lg font-bold"
-                      >
-                        XL
-                      </option>
-                    </select>
+                    <button>
+                      <i className="bx bxs-circle text-primary-color"></i>
+                    </button>
+                    <button>
+                      <i className="bx bxs-circle text-[#23856D]"></i>
+                    </button>
+                    <button>
+                      <i className="bx bxs-circle text-[#E77C40]"></i>
+                    </button>
+                    <button>
+                      <i className="bx bxs-circle text-[#252B42]"></i>
+                    </button>
                   </div>
-                  <Button className="w-52 bg-orange-500">Sepete Ekle</Button>
-                </CardFooter>
-              </Card>
-            </Link>
-          ))} */}
+                </Typography>
+                <Typography
+                  variant="h5"
+                  color="blue-gray"
+                  className="mb-2 line-clamp-4 font-normal text-base"
+                  id={product.rating}
+                >
+                  <span className="text-orange-600 font-bold">
+                    {" "}
+                    {product.rating}{" "}
+                  </span>
+                  <span className="text-gray-600 text-opacity-75">
+                    times was added to fav
+                  </span>
+                </Typography>
+                <span className="font-bold text-orange-800 ">
+                  {product.price}₺
+                </span>
+              </CardBody>
+            </Card>
+          ))}
         </div>
       </div>
     </div>
