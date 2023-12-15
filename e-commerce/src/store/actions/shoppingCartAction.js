21c3dce627cf7147296a3ec2425eb74636a2aa6c
Gulbeyaz <glbzbayram@gmail.com>
2023-12-15 13:07:23 +0300

    has been added new actions for checkbox and total Quantity

--- e-commerce/src/store/actions/shoppingCartAction.js
+++ e-commerce/src/store/actions/shoppingCartAction.js
@@ -2,9 +2,24 @@ export const SET_CART_LIST = "SET_CART_LIST";
 export const SET_PAYMENT = "SET_PAYMENT";
 export const SET_ADDRESS = "SET_ADDRESS";
 export const UPDATE_QUANTITY = "UPDATE_QUANTITY";
+export const SET_TOTALPRICE = "SET_TOTALPRICE";
+export const SET_TOTALQUANTITY = "SET_TOTALQUANTITY";
+export const UPDATE_TOTAL_AMOUNT = "UPDATE_TOTAL_AMOUNT";
 
 export const setCartListAction = (cartList) => {
   return { type: SET_CART_LIST, payload: cartList };
+};
+
+export const setTotalPriceAction = () => {
+  return { type: SET_TOTALPRICE };
+};
+
+export const updateTotalPriceAction = (checkedState, id) => {
+  return { type: UPDATE_TOTAL_AMOUNT, payload: checkedState, id };
+};
+
+export const setTotalQuantityAction = () => {
+  return { type: SET_TOTALQUANTITY };
 };
 
 export const updateQuantityAction = (type, id) => {
