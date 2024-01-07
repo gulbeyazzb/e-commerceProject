a0fc3b0dda3de2e639c1d1653beb5dbc9145fde8
Gulbeyaz <glbzbayram@gmail.com>
2024-01-07 23:48:41 +0300

    Payment page

--- e-commerce/src/pages/PaymentPage.js
+++ e-commerce/src/pages/PaymentPage.js
@@ -14,6 +14,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 import Payment from "../components/Payment";
+import OrderSummary from "../components/OrderSummary";
 
 const PaymentPage = () => {
   const [spinner, setSpinner] = useState(false);
@@ -75,50 +76,53 @@ const PaymentPage = () => {
   }, [newAddress]);
 
   return (
-    <div className="w-[1140px] m-auto ">
-      <ToastContainer />
-      {/* ----------------------ADDRESS OR PAYMENT HEADER SECTION--------------------------------------------------- */}
-      <div className="mx-12 flex justify-center gap-1 h-[140px]">
-        <NavLink
-          to="/order"
-          className=" border border-gray-400 text-gray-600 pt-3 bg-gray-300 shadow-lg p-3 rounded-md text-xs w-[48%] "
-        >
-          <h1 className="text-2xl font-extrabold text-gray-700">
-            Address Info
-          </h1>
-          <div className="flex flex-col text-xs">
-            <div className="flex justify-between items-center">
-              <h5 className=" font-bold flex items-center text-black gap-1">
-                <i className="bx bxs-user text-gray-700 h-full text-lg"></i>{" "}
-                {selectedAddress?.name + " " + selectedAddress?.surname}
-              </h5>
-              <h5 className=" font-bold flex items-center text-black gap-1">
-                <i className="bx bxs-phone h-full text-lg"></i>{" "}
-                {selectedAddress?.phone.slice(0, 3) +
-                  "**********" +
-                  selectedAddress?.phone.slice(8, 10)}
+    <div className="w-[1150px] mx-auto flex justify-between">
+      <div className="w-[105%] me-2 sm:py-10 ">
+        <ToastContainer />
+        {/* ----------------------ADDRESS OR PAYMENT HEADER SECTION--------------------------------------------------- */}
+        <div className="ms-16 flex justify-center gap-1 h-[140px]">
+          <NavLink
+            to="/order"
+            className="border border-b-4 pt-3  shadow-lg p-3 rounded-md text-lg w-1/2 border-gray-400 text-gray-600  bg-gray-300  "
+          >
+            <h1 className="text-lg font-extrabold text-gray-700">
+              Address Info
+            </h1>
+            <div className="flex flex-col text-xs">
+              <div className="flex justify-between items-center">
+                <h5 className=" font-bold flex items-center text-black gap-1">
+                  <i className="bx bxs-user text-gray-700 h-full text-lg"></i>{" "}
+                  {selectedAddress?.name + " " + selectedAddress?.surname}
+                </h5>
+                <h5 className=" font-bold flex items-center text-black gap-1">
+                  <i className="bx bxs-phone h-full text-lg"></i>{" "}
+                  {selectedAddress?.phone.slice(0, 3) +
+                    "**********" +
+                    selectedAddress?.phone.slice(8, 10)}
+                </h5>
+              </div>
+              <div className="flex flex-col">
+                <p className=" ">{selectedAddress?.address}</p>
+                <span className=" ">
+                  {selectedAddress?.district + "/" + selectedAddress?.city}
+                </span>
+              </div>
+            </div>
+          </NavLink>
+          <div className="cursor-pointer border border-b-4 border-b-orange-700 border-gray-400 text-gray-600 pt-3 bg-gray-100 shadow-lg p-6 rounded-md w-1/2  ">
+            <h1 className="text-lg font-extrabold text-orange-700">Payment</h1>
+            <div className="flex flex-col ">
+              <h5 className="py-2 text-xs font-bold  text-gray-500">
+                You can make your payment safely by{" "}
+                <span className="text-black">Debit/Credit Card</span> or{" "}
+                <span className="text-black">Shopping Credit.</span>
               </h5>
             </div>
-            <div className="flex flex-col">
-              <p className=" ">{selectedAddress?.address}</p>
-              <span className=" ">
-                {selectedAddress?.district + "/" + selectedAddress?.city}
-              </span>
-            </div>
-          </div>
-        </NavLink>
-        <div className="cursor-pointer border border-b-4 border-b-orange-700 border-gray-400 text-gray-600 pt-3 bg-gray-100 shadow-lg p-6 rounded-md w-[48%]  ">
-          <h1 className="text-2xl font-extrabold text-orange-700">Payment</h1>
-          <div className="flex flex-col ">
-            <h5 className="py-2 text-xs font-bold  text-gray-500">
-              You can make your payment safely by{" "}
-              <span className="text-black">Debit/Credit Card</span> or{" "}
-              <span className="text-black">Shopping Credit.</span>
-            </h5>
           </div>
         </div>
+        <Payment selectAddressHandle={selectAddressHandle} />
       </div>
-      <Payment selectAddressHandle={selectAddressHandle} />
+      <OrderSummary />
     </div>
   );
 };
